9d58f5bc7d16664ed5b4edf53423de4a2a6579f4
malesch <malesch@gmail.com>
2016-08-20 19:31:42 +0200

    First basic test cases

--- test/croque/core_test.clj
+++ test/croque/core_test.clj
@@ -1,7 +1,31 @@
 (ns croque.core-test
   (:require [clojure.test :refer :all]
-            [croque.core :refer :all]))
+            [croque.core :refer :all]
+            [croque.appender :as appender]
+            [croque.test-utils :refer [with-components]]
+            [croque.appender :as appender]
+            [croque.tailer :as tailer]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest creation-test
+  (testing "Creation of queue, appender and tailer instances"
+    (with-components [queue (new-croque-queue {:path "target/creation"})]
+      (let [appender (create-appender queue)
+            tailer (create-tailer queue)]
+        (is (map? (state queue)) "No queue state available")
+        (is (map? (appender/state appender)) "No appender state available")
+        (is (map? (tailer/state tailer)) "No tailer state available")))))
+
+(deftest simple-append-peek-test
+  (testing "Create queue, append entries and peek test messages"
+    (with-components [queue (new-croque-queue {:path "target/append-peek"})]
+      (let [appender (create-appender queue)
+            tailer (create-tailer queue)]
+        ;; add three messages
+        (appender/append! appender {:foo 1})
+        (appender/append! appender {:foo 2})
+        (appender/append! appender {:foo 3})
+        (is (= 2 (:last-sequence-appended (appender/state appender))) "Wrong appender index")
+        (is (= {:foo 1} (tailer/peek tailer)) "Wrong first message")
+        (is (= {:foo 2} (tailer/peek tailer)) "Wrong second message")
+        (is (= {:foo 3} (tailer/peek tailer)) "Wrong third message")
+        (is (nil? (tailer/peek tailer)) "No message expected")))))
--- test/croque/test_utils.clj
+++ test/croque/test_utils.clj
@@ -1,1 +1,29 @@
 (ns croque.test-utils)
+
+(defmacro ^{:private true} assert-args
+  [& pairs]
+  `(do (when-not ~(first pairs)
+         (throw (IllegalArgumentException.
+                  (str (first ~'&form) " requires " ~(second pairs) " in " ~'*ns* ":" (:line (meta ~'&form))))))
+       ~(let [more (nnext pairs)]
+          (when more
+            (list* `assert-args more)))))
+
+(defmacro with-components
+  "Create component bindings which are started and finally stopped again.
+  In order to use this macro, the namespace of Stuarts components library
+  must of course be present (mainly copied from the `with-open` macro)."
+  [bindings & body]
+  (assert-args
+    (vector? bindings) "a vector for its binding"
+    (even? (count bindings)) "an even number of forms in binding vector")
+  (cond
+    (= (count bindings) 0) `(do ~@body)
+    (symbol? (bindings 0)) `(let [~(bindings 0)
+                                  (com.stuartsierra.component/start ~(bindings 1))]
+                              (try
+                                (with-components ~(subvec bindings 2) ~@body)
+                                (finally
+                                  (com.stuartsierra.component/stop ~(bindings 0)))))
+    :else (throw (IllegalArgumentException.
+                   "with-components only allows Symbols in bindings"))))
