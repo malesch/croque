701830311fc88b728ec7ef406d3b6813917dd284
malesch <malesch@gmail.com>
2016-08-20 20:52:58 +0200

    Add logging deps

--- project.clj
+++ project.clj
@@ -4,7 +4,13 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha10"]
                  [com.stuartsierra/component "0.3.1"]
+                 ;; Logging
                  [com.taoensso/timbre "4.7.0"]
+                 [ch.qos.logback/logback-classic "1.1.7" :exclusions [org.slf4j/slf4j-api]]
+                 [org.slf4j/jul-to-slf4j "1.7.21"]
+                 [org.slf4j/jcl-over-slf4j "1.7.21"]
+                 [org.slf4j/log4j-over-slf4j "1.7.21"]
+                 ;; Chronicle Queue
                  [net.openhft/chronicle-queue "4.5.12"]]
   :profiles {:dev {:source-paths ["dev"]
                    :dependencies [[org.clojure/tools.namespace "0.2.11"]]}})
