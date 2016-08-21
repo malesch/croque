6654a55c4294225e664b43962b1aa78c4f719ba2
malesch <malesch@gmail.com>
2016-08-21 16:03:13 +0200

    Additional tests

--- test/croque/core_test.clj
+++ test/croque/core_test.clj
@@ -29,3 +29,40 @@
         (is (= {:foo 2} (tailer/next tailer)) "Wrong second entry")
         (is (= {:foo 3} (tailer/next tailer)) "Wrong third entry")
         (is (nil? (tailer/next tailer)) "No next entry expected")))))
+
+(deftest rewind-test
+  (testing "Rewind function"
+    (with-components [queue (new-croque-queue {:path (random-path)})]
+      (let [appender (create-appender queue)
+            tailer (create-tailer queue)]
+        (appender/append! appender :1st-entry)
+        (appender/append! appender :2nd-entry)
+        (tailer/next tailer)                                ;; :1st-entry
+        (tailer/next tailer)                                ;; :2nd-entry
+        (tailer/rewind tailer 2)
+        (is (= :1st-entry (tailer/next tailer)) "Wrong first entry after rewind")))))
+
+(deftest seek-sequence-position-test
+  (testing "Seek sequence position function"
+    (with-components [queue (new-croque-queue {:path (random-path)})]
+      (let [appender (create-appender queue)
+            tailer (create-tailer queue)]
+        (appender/append! appender [:entry {:num 1}])
+        (appender/append! appender [:entry {:num 2}])
+        (tailer/next tailer)                                ;; [:entry {:num 1}]
+        (tailer/next tailer)                                ;; [:entry {:num 2}]
+        (tailer/seek-sequence-position tailer 0)
+        (is (= [:entry {:num 1}] (tailer/next tailer)) "Wrong first entry after seek sequence position")))))
+
+(deftest seek-index-position-test
+  (testing "Seek index position function"
+    (with-components [queue (new-croque-queue {:path (random-path)})]
+      (let [appender (create-appender queue)
+            tailer (create-tailer queue)]
+        (appender/append! appender #{:first-entry})
+        (let [idx (:last-index-appended (appender/state appender))]
+          (appender/append! appender #{:second-entry})
+          (tailer/next tailer)                              ;; #{:first-entry}
+          (tailer/next tailer)                              ;; #{:second-entry}
+          (tailer/seek-index-position tailer idx)
+          (is (= #{:first-entry} (tailer/next tailer)) "Wrong first entry after seek index position"))))))
